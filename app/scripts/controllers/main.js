35c7f37cc81b4a9cfeb9b12c98f59abea716f0fc
Sarath <sarath.hacks@gmail.com>
2014-12-12 14:47:32 +0530

    Updated files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,32 @@
  * Controller of the animatorApp
  */
 angular.module('animatorApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$rootScope,$location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $scope.pageClass="page";
+
+    // Go Method
+    $rootScope.go = function (path, pageAnimationClass) {
+	 
+	    if (typeof(pageAnimationClass) === 'undefined') { // Use a default, your choice
+	        $rootScope.pageAnimationClass = 'crossFade';
+	    }
+	         
+	    else { // Use the specified animation
+	        $rootScope.pageAnimationClass = pageAnimationClass;
+	    }
+	 
+	    if (path === 'back') { // Allow a 'back' keyword to go to previous page
+	        $window.history.back();
+	    }
+	         
+	    else { // Go to the specified path
+	        $location.path(path);
+	    }
+	};
+    //End Go method
   });
